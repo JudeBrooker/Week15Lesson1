5b7d353e0800f87117455ccb57ef70fc8cc9d1d9
Jude <91631@farnborough.ac.uk>
2020-01-06 11:38:10 +0000

    favTopics

---
+++ src/favTopics.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+public class favTopics {
+    public static void main(String[] args){
+        ArrayList<String> myFavTopics = new ArrayList<>();
+        myFavTopics.add("HTML&CSS");
+        myFavTopics.add("Procedural");
+        myFavTopics.add("Internal workings");
+        myFavTopics.add("Types of processor");
+        myFavTopics.add("Logic gates");
+        myFavTopics.add("Binary and hex");
+        myFavTopics.add("File handling");
+        myFavTopics.add("Arrays and searches");
+        myFavTopics.add("String handling");
+        myFavTopics.add("Types of network");
+        myFavTopics.add("Devices & storage");
+        myFavTopics.add("TCP/IP & DNS");
+        myFavTopics.add("Legislation");
+        System.out.println(myFavTopics);
+    }
+}
